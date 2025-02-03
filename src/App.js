25a822d297dee8f3bc5224bb2498aa0e0505c7f9
ShashiRV <shashir68619@gmail.com>
2025-02-03 17:09:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Peer-Mentor Learning Series Workshop 1</h1>
+        <h1>Kissik Learning Series Workshop 1</h1>
         <div className="user-info">
           <p><strong>Name:</strong> Your Name</p> 
           <p><strong>USN:</strong> Your USN</p>
